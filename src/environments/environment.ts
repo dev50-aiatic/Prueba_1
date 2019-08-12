e91dcac6952a0adc2eb80eede7ecdaeb18342cab
carlos daniel pabon jaimes <dev50@aiatic.com>
2019-08-12 07:53:41 -0500

    y ahora?

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  
   firebase:{
     apiKey: "AIzaSyChr8H4IRPxQ0eo3jkn4jzT4gQozLJkoJw",
     authDomain: "subida-37b9b.firebaseapp.com",
