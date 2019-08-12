780f87cdc97184d8d1de56c40d6eaad562c020f9
carlos daniel pabon jaimes <dev50@aiatic.com>
2019-08-12 07:49:57 -0500

    pruebaxxx

--- src/app/components/perfiles/perfil/perfil.component.ts
+++ src/app/components/perfiles/perfil/perfil.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PerfilService } from '../../../services/perfil.service'
 import { NgForm } from '@angular/forms';
 import { Perfil } from 'src/app/models/perfil';
-import { AngularFireStorage } from '@angular/fire/storage';
-import { finalize } from 'rxjs/operators';
+
  
 @Component({
   selector: 'app-perfil',
@@ -12,7 +11,7 @@ import { finalize } from 'rxjs/operators';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor(private perfilService: PerfilService, private storage: AngularFireStorage) { }
+  constructor(private perfilService: PerfilService,) { }
 
  
 
@@ -34,13 +33,7 @@ export class PerfilComponent implements OnInit {
     perfilForm.reset();
     this.perfilService.seleccionarPerfil = new Perfil();
   }
-  subir (e){
-    const id = Math.random().toString(36).substring(2);
-    const file = e.target.files[0];
-    const filePath = 'uploads/profile_${id}';
-    const ref = this.storage.ref(filePath);
-    const task = this.storage.upload(filePath, file);
-
+  
     
   }
-}
+
