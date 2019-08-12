9e7ec2dec1efef27a5ca4770d9178063c0f38f72
carlos daniel pabon jaimes <dev50@aiatic.com>
2019-08-12 08:08:05 -0500

    sirvio?

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyChr8H4IRPxQ0eo3jkn4jzT4gQozLJkoJw",
+    authDomain: "subida-37b9b.firebaseapp.com",
+    databaseURL: "https://subida-37b9b.firebaseio.com",
+    projectId: "subida",
+    storageBucket: "subida-37b9b.appspot.com",
+    messagingSenderId: "432812695101",
+    }
 };
