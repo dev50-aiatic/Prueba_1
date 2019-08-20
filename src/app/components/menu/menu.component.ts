b756472c2182c579628b76821cad2fd9a5032317
carlos daniel pabon jaimes <dev50@aiatic.com>
2019-08-20 18:12:22 -0500

    "Prueba_Host_4"
    Version final funcional

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { PerfilService } from '../../services/perfil.service';
-import { AngularFireAuth } from '@angular/fire/auth';
 
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
-  constructor() { }
+  estado = false;
+  constructor(private menuService:PerfilService , private router:Router) { }
   
   
   ngOnInit() {
    
   }
-  
+  ngDoCheck(): void {
+   
+    this.estado= this.menuService.status;
 
+  }
+  Desconexion() {
+    this.estado = false;
+  }
 }
 
