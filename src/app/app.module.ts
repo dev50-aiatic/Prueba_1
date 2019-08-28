55cab16304ec75d4d0a805d8e73c9c533525d856
carlos daniel pabon jaimes <dev50@aiatic.com>
2019-08-28 16:07:03 -0500

    "Prueba_SLgoogle_v1.1"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,7 +36,7 @@ import { GoogleLoginProvider, FacebookLoginProvider } from "angularx-social-logi
 let config = new AuthServiceConfig([
   {
     id: GoogleLoginProvider.PROVIDER_ID,
-    provider: new FacebookLoginProvider("20148850796-ocjpc22ic574vjlur34i3r7bblruda4o.apps.googleusercontent.com")
+    provider: new GoogleLoginProvider("20148850796-ocjpc22ic574vjlur34i3r7bblruda4o.apps.googleusercontent.com")
   },
   {
     id: FacebookLoginProvider.PROVIDER_ID,
