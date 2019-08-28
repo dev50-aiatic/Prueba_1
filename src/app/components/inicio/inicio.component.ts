06cac4c37307e920a19b3240d044f80275c33c47
carlos daniel pabon jaimes <dev50@aiatic.com>
2019-08-28 06:48:45 -0500

    "Prueba_SL_v4.2"

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -12,12 +12,24 @@ import { Perfil } from '../../models/perfil';
 export class InicioComponent implements OnInit {
   usuarioPerfil:Perfil;
   
-  
+  estado = false;
+  url:string;
+  nombre:string;
+  correo:string;
+  tipoCuenta:boolean;
 
   constructor(private perfilService:PerfilService, private router:Router) { }
     ngOnInit() {
       this.usuarioPerfil = this.perfilService.usuarioServicio;
-      
+      if(this.perfilService.identificacionCuenta()=='interno'){
+        this.usuarioPerfil = this.perfilService.usuarioServicio;
+        this.tipoCuenta = true;
+      }else if (this.perfilService.identificacionCuenta()=='facebook') {
+        this.nombre = this.perfilService.user.name;
+        this.correo = this.perfilService.user.email;
+        this.url = this.perfilService.user.photoUrl;
+        this.tipoCuenta = false;
+    }
       
   }
 }
