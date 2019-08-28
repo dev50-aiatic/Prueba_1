b1ea43742db3dae2de30f82e9cb9b51142f955ac
carlos daniel pabon jaimes <dev50@aiatic.com>
2019-08-28 09:47:53 -0500

    "Prueba_BD-SL_v4.8"

--- src/app/components/perfiles/registro/registro.component.ts
+++ src/app/components/perfiles/registro/registro.component.ts
@@ -10,8 +10,16 @@ import { Perfil } from 'src/app/models/perfil';
 })
 export class RegistroComponent implements OnInit {
   
+  //Verificacion de Login
   @Output() valido = false;
+
+  //Estado de conexión
+  estado = false;
+
+  //Almacenamiento de usuario
   usuarios:Perfil[] = [];
+  
+  //Variables para usuario con login local
   nombre:string;
   nick:string;
   nacimiento:Date;
@@ -19,72 +27,61 @@ export class RegistroComponent implements OnInit {
   identificacion:number;
   usuarioPerfil:Perfil;
   
-  estado = false;
+  //Variables para usuarios con SocialLogil
+  
   url:string;
   nombrers:string;
   correo:string;
   tipoCuenta:boolean;
+
   constructor(private registroServices:PerfilService, private router: Router) { }
 
   ngOnInit() {
+    
+    //Inicializacion de arreglo con usuarios existentes
     this.registroServices.obtenerUsuarios().subscribe((Perfil:Perfil[])=>{this.registroServices.setUsuarios(Perfil)});
+    
+    //Conexion del vector con el servicio
     this.usuarios = this.registroServices.usuarios;
+    
+    //Estatus de conexión
     if(this.registroServices.status==true){
       this.router.navigate(['/inicio'])
     }
-    if(this.registroServices.identificacionCuenta()=='interno'){
-      this.usuarioPerfil = this.registroServices.usuarioServicio;
-      this.tipoCuenta = true;
-    }else if (this.registroServices.identificacionCuenta()=='facebook') {
-      this.nombre = this.registroServices.user.name;
-      this.correo = this.registroServices.user.email;
-      this.url = this.registroServices.user.photoUrl;
-      this.tipoCuenta = false;
+
   }
-  }
+  //Crear usuario de forma local
   onAgregarUsuario(){
     console.log("el nombre es: "+this.nombre+"\nel nick es: "+this.nick+"\nel nacimiento es: "+
     this.nacimiento+ "\n la identificacion es:"+this.identificacion);
     this.registroServices.onValidacionid(this.identificacion);
     this.valido = this.registroServices.valido;
-    if(this.valido===true){
-    console.log("es valido");
-      this.registroServices.onAgregar(new Perfil(this.nombre, this.nick, this.nacimiento, this.contrasena,this.identificacion));
-      this.router.navigate(['/login']);
-      
-    }else{
-    this.router.navigate(['/registro']);
-    alert("identificacion en uso");
+        if(this.valido===true){
+        console.log("es valido");
+          this.registroServices.onAgregar(new Perfil(this.nombre, this.nick, this.nacimiento, this.contrasena,this.identificacion));
+          this.router.navigate(['/login']); 
+        }else{
+        this.router.navigate(['/registro']);
+        alert("identificacion en uso");
+        }  
     }
-    
-     
-  }
+  
+  //Agregar usuario mediante FB  
   onAgregarUsuarioFB(){
     this.registroServices.signInWithFB();
     this.estado = this.registroServices.status;
     this.nombrers = this.registroServices.user.name;
-      this.correo = this.registroServices.user.email;
-      this.url = this.registroServices.user.photoUrl;
-      this.tipoCuenta = false;
-    if (this.estado === true){
-      console.log("es valido");
-      this.registroServices.onAgregar(new Perfil(this.nombrers,this.correo));
-      console.log(this.nombrers);
-    }
-    
-    //this.registroServices.onValidacionid(this.identificacion);
-    //this.valido = this.registroServices.valido;
-    //if(this.valido===true){
-    
-    //  this.router.navigate(['/login']);
-      
-    else{
-    this.router.navigate(['/registro']);
-    alert("pailas en uso");
-    }
-    
-     
-  }
-
-  
+    this.correo = this.registroServices.user.email;
+    this.url = this.registroServices.user.photoUrl;
+    this.tipoCuenta = false;
+        if (this.estado === true){
+          console.log("conecto");
+          this.registroServices.onAgregar(new Perfil(this.nombrers,this.correo));
+          console.log(this.nombrers,this.correo,this.url);
+        } 
+        else{
+        this.router.navigate(['/registro']);
+        alert("pailas en uso");
+        }   
+  } 
 }
--- src/app/models/perfil.ts
+++ src/app/models/perfil.ts
@@ -1,5 +1,12 @@
 
 export class Perfil{
-constructor (public nombre?:string, public usuario?:string,public nacimiento?:Date, public contrasena?:string, public identificacion?:number){}
+constructor (
+    public nombre?:string, 
+    public usuario?:string,
+    public nacimiento?:Date, 
+    public contrasena?:string, 
+    public identificacion?:number,
+    
+    ){}
 }
  
